8add5cadabb4897622e915c3a246c1aba3b0c094
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-27 19:38:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Juan Restrepo"
+  end
+
+  get '/hometown' do
+  "My hometown is Santa Rosa"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Tu carcel"
+  end
+
 end
